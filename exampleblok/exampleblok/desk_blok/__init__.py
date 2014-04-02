7e6420d9bf23ebaeab7ffcb58f9fe5836a1a85ee
jssuzanne <devnull@localhost>
2014-04-02 09:38:43 +0200

    Add doc string

--- exampleblok/exampleblok/desk_blok/__init__.py
+++ exampleblok/exampleblok/desk_blok/__init__.py
@@ -4,6 +4,7 @@ from anyblok.blok import Blok
 class DeskBlok(Blok):
 
     def install(self):
+         """ this method is call at the installation of this blok """
         address = self.registry.Address.insert(street='14-16 rue Soleillet',
                                                zip='75020', city='Paris')
         self.registry.Room.insert(number=308, address=address)
