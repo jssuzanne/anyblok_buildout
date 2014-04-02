681206e2e9e413da25e3a4d8e90607599a30bcd7
jssuzanne <devnull@localhost>
2014-04-02 12:59:48 +0200

    Add install method in worker_position_blok

--- exampleblok/exampleblok/position_blok/__init__.py
+++ exampleblok/exampleblok/position_blok/__init__.py
@@ -4,6 +4,6 @@ from anyblok.blok import Blok
 class PositionBlok(Blok):
 
     def install(self):
-        for position in ('DG', 'Cormercial', 'Secrétaire', 'Chef de projet',
+        for position in ('DG', 'Comercial', 'Secrétaire', 'Chef de projet',
                          'Developper'):
             self.registry.Position.insert(name=position)
--- exampleblok/exampleblok/worker_position_blok/__init__.py
+++ exampleblok/exampleblok/worker_position_blok/__init__.py
@@ -4,7 +4,27 @@ from anyblok.blok import Blok
 
 class WorkerPositionBlok(Blok):
 
+    priority = 200
+
     conditional = [
         'worker',
         'position',
     ]
+
+    def install(self):
+        Worker = self.registry.Worker
+
+        position_by_worker = {
+            'Georges Racinet': 'DG',
+            'Christophe Combelles': 'Comercial',
+            'Sandrine Chaufournais': u"Secrétaire",
+            'Pierre Verkest': 'Chef de projet',
+            u"Simon André": 'Developper',
+            'Florent Jouatte': 'Developper',
+            'Clovis Nzouendjou': 'Developper',
+            u"Jean-Sébastien Suzanne": 'Developper',
+        }
+
+        for worker, position in position_by_worker.items():
+            Worker.query().filter(Worker.name == worker).update({
+                'position_name': position})
