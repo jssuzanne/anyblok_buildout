59a19d8a761fe91fe12f75b0a556ab88fc0a4a7f
jssuzanne <devnull@localhost>
2014-04-06 00:39:01 +0200

    Update the script

--- exampleblok/exampleblok/scripts.py
+++ exampleblok/exampleblok/scripts.py
@@ -6,12 +6,20 @@ logger = getLogger(__name__)
 
 
 def exampleblok():
+    # Initialise the application, with a name and a version number
+    # select the groupe of options to display
+    # select the groups of bloks availlable
+    # return a registry if the database are selected
     registry = anyblok.start(
         'Example Blok', '1.0',
-        argsparse_groups=['config', 'database', 'message', 'logging'],
+        argsparse_groups=['config', 'database', 'message'],
         parts_to_load=['AnyBlok', 'WorkBlok'])
     message_before = ArgsParseManager.get('message_before')
     message_after = ArgsParseManager.get('message_after')
+
+    if not registry:
+        logger.warning("No database database selected")
+        exit(0)
 
     if message_before:
         logger.info(message_before)
