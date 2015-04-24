678ff1990bd03bfccd639563200e964f26385e99
jssuzanne <devnull@localhost>
2015-04-24 12:12:39 +0200

    rm nose.sh outdated

--- nose.sh
+++
@@ -1,4 +0,0 @@
-./bin/buildout -c buildout.cfg
-./bin/nosetests anyblok/anyblok/tests anyblok/anyblok/bloks/anyblok_core/tests -s -v --with-coverage --cover-package anyblok
-./bin/buildout -c anyblok_web_server.cfg
-./bin/nosetests anyblok_web_server/anyblok_web_server/bloks/web/tests -s -v
